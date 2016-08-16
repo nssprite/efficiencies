7919a256983d3e0575d7381bf458ce56917bf946
YangXiaoshan <nssprite@gmail.com>
2016-08-16 23:02:11 +0800

    support search stock by short name

--- stock.py
+++ stock.py
@@ -2,7 +2,9 @@
 # -*- coding:utf-8 -*-
 
 """
-api: http://api.money.126.net/data/feed/0600749,money.api
+api:
+stock detail: http://api.money.126.net/data/feed/0600749,money.api
+search stock: http://quotes.money.163.com/stocksearch/json.do?type=&count=10&callback=Baidu.Finance.Suggest.GetResult&word=zpgf
 """
 
 import sys
@@ -14,6 +16,7 @@ import gzip
 import cStringIO
 
 default_list = ['000001', '600749', '002325', '603883']
+referer = 'http://www.hao123.com/stocknew'
 
 def create_request(url, referer = None):
     req = urllib2.Request(
@@ -32,14 +35,17 @@ def create_request(url, referer = None):
     return req
 
 def show_help():
-    print "Usage: python ./stock.py [000001]"
+    print "Usage:"
+    print "python ./stock.py -h[--help] show this help"
+    print "python ./stock.py            show default stock's quotation"
+    print "python ./stock.py -c 000001  show specify stock's quotation"
+    print "python ./stock.py -s gyyh    search the no of stock by short name"
     sys.exit()
 
 def get_stock(stock_list):
-    print time.ctime()
+    request_time = time.ctime()
     stock_str = ','.join(stock_list)
     api_url = "http://api.money.126.net/data/feed/%s,money.api" % stock_str
-    referer = 'http://www.hao123.com/stocknew'
     f = urllib2.urlopen(create_request(api_url, referer), timeout=15)
     data = f.read()
     if data[:6] == '\x1f\x8b\x08\x00\x00\x00':
@@ -50,6 +56,7 @@ def get_stock(stock_list):
         json_data = data[(data.index('(') + 1): -2]
         json_list = json.loads(json_data)
 
+        print request_time
         for i in json_list:
             stock = json_list[i]
             symbol = stock['symbol'].encode('utf-8')
@@ -60,19 +67,40 @@ def get_stock(stock_list):
             arrow = stock['arrow']
             percent = str(round((100 * percent), 2)) + "%"
             print symbol, "\t", price, "\t", updown, "\t", percent, "\t", arrow
-    
+
+def search_stock(short_name):
+    api_url = "http://quotes.money.163.com/stocksearch/json.do?type=&count=10&callback=Baidu.Finance.Suggest.GetResult&word=%s" % short_name
+    f = urllib2.urlopen(create_request(api_url, referer), timeout=15)
+    data = f.read()
+    if data[:6] == '\x1f\x8b\x08\x00\x00\x00':
+        data = gzip.GzipFile(fileobj = cStringIO.StringIO(data)).read()
+    f.close()
+
+    if data is not None:
+        json_data = data[(data.index('(') + 1): -1]
+        json_list = json.loads(json_data)
+
+        for stock in json_list:
+            symbol = stock['symbol'].encode('utf-8')
+            name = stock['name'].encode('utf-8')
+            print symbol, "\t", name
+
 if __name__ == '__main__':
     if (len(sys.argv) > 1 and (sys.argv[1] == '-h' or sys.argv[1] == '--help')):
         show_help()
 
     if len(sys.argv) > 1:
-        if len(sys.argv[1]) != 6:
+        # search stock by short name
+        if sys.argv[1] == '-s' and sys.argv[2].isalpha():
+            search_stock(sys.argv[2])
+            sys.exit()
+        # get current quotation of specify stock
+        if sys.argv[1] == '-c' and sys.argv[2].isdigit():
+            daemon = False
+            stocks = []
+            stocks.append(sys.argv[2])
+        else:
             show_help()
-        if sys.argv[1].isdigit() is None:
-            show_help()
-        daemon = False
-        stocks = []
-        stocks.append(sys.argv[1])
     else:
         daemon = True
         stocks = default_list
@@ -91,6 +119,6 @@ if __name__ == '__main__':
         while True:
             get_stock(stock_list)
             print os.linesep*20
-            time.sleep(5)
+            time.sleep(2)
     else:
         get_stock(stock_list)
